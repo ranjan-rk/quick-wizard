e2370d16a700bf8a8cf6c858482287204ff6a3e9
Ranjan Kumar <ranjan.rk@gmail.com>
2011-11-22 14:53:48 +0530

    initial checkin---added maven modules for web, service and persistence

---
+++ quick-wizard-persistence/src/main/java/org/quick/wizard/persistence/App.java
@@ -0,0 +1,13 @@
+package org.quick.wizard.persistence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
