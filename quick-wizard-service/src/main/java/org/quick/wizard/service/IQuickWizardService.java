ea13f6ad51b3f578c4b8c76984421998bb7e5ad4
Ranjan Kumar <ranjan.rk@gmail.com>
2011-12-16 15:30:52 +0530

    cxf config changes and added a sample service api for cxf

--- quick-wizard-service/src/main/java/org/quick/wizard/service/IQuickWizardService.java
+++ quick-wizard-service/src/main/java/org/quick/wizard/service/IQuickWizardService.java
@@ -1,5 +1,16 @@
 package org.quick.wizard.service;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/qw")
+@Consumes( { "application/xml", "application/json" })
+@Produces( { "application/xml", "application/json" })
 public interface IQuickWizardService {
 
+	@GET
+	@Path("/appname")
+	String getApplicationName();
 }
