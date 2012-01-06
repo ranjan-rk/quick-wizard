d81a9724523ca50e12e5939a5bb30ac107ba6531
Ranjan Kumar <ranjan.rk@gmail.com>
2012-01-06 17:52:51 +0530

    added missing files

---
+++ quick-wizard-persistence/src/main/java/org/quick/wizard/persistence/framework/hibernate/HibernatePersistence.java
@@ -0,0 +1,46 @@
+package org.quick.wizard.persistence.framework.hibernate;
+
+import java.util.List;
+
+import org.quick.wizard.persistence.framework.IPersistence;
+import org.springframework.data.mongodb.core.query.Query;
+
+public class HibernatePersistence implements IPersistence {
+
+    @Override
+    public void delete(Object entity) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<?> find(Query query, Class<?> clazz) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public List<?> findAll(Class<?> clazz) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public Object findOne(Query query, Class<?> clazz) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public Object save(Object entity) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public Iterable<Object> save(Iterable<? extends Object> entities) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
